1c0ab5a83459802faa2f40c459ea499949d9906b
pro-devel-linux <saber20_13@hotmail.com>
2017-07-24 13:23:19 +0200

    version 3

--- main.c
+++ main.c
@@ -1,51 +1,104 @@
-#include <stdio.h>
-#include "stdlib.h"
+#include "mpc.h"
+#include <math.h>
 
-// for compiling on windows I use fputs and gets since windows already using arrow editing
 #ifdef _WIN32
-#include <string.h>
 
 static char buffer[2048];
 
-// fake readline function
 char* readline(char* prompt) {
-	fputs(prompt, stdout);
-	fgest(buffer, 2048, stdin);
-	char* cpy = malloc(strlen(buffer)+1);
-	cpy[strlen(cpy)-1] = '\0';
-	return cpy;
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
 }
 
-// fake add history function
 void add_history(char* unused) {}
 
-//otherwise compling on mac or linux include readline library.
 #else
-#include "editline/readline.h"
-#include "editline/history.h"
+#include <editline/readline.h>
+#include <editline/history.h>
 #endif
+
+/* Use operator string to see which operation to perform */
+long eval_op(long x, char* op, long y) {
+  if (strcmp(op, "+") == 0) { return x + y; }
+  if (strcmp(op, "-") == 0) { return x - y; }
+  if (strcmp(op, "*") == 0) { return x * y; }
+  if (strcmp(op, "/") == 0) { return x / y; }
+  if (strcmp(op, "^") == 0) { return pow(x,y); }
+  return 0;
+}
+
+long eval(mpc_ast_t* t) {
+
+  /* If tagged as number return it directly. */
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+
+  /* The operator is always second child. */
+  char* op = t->children[1]->contents;
+
+  /* We store the third child in `x` */
+  long x = eval(t->children[2]);
+
+  /* Iterate the remaining children and combining. */
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
+}
 
 int main(int argc, char** argv) {
   
-	// print version and exit information
-	float ver = 0.2;
-	printf("SHD version %.1f \n",ver);
-	puts("Press ctrl+c to Exit\n");
-
-	// In a never ending loop
-	while (1) {
-		// output a prompt then get input again
-		char* input = readline("SHD> ");
-
-		//  user enteries are now stored & can calledback with arrows
-		add_history(input);
-
-		// echo input back to user
-		printf("No you're a %s\n", input);
-
-		// free retrieved input
-		free(input);
-	}
-	
+  /* Create Some Parsers */
+  mpc_parser_t* Number   = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr     = mpc_new("expr");
+  mpc_parser_t* Shd    = mpc_new("shd");
+  
+  /* Define them with the following Language */
+  mpca_lang(MPCA_LANG_DEFAULT,
+    "                                                     \
+      number   : /-?[0-9]+/ ;                             \
+      operator : '+' | '-' | '*' | '/' | '^' ;                  \
+      expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+      shd    : /^/ <operator> <expr>+ /$/ ;             \
+    ",
+    Number, Operator, Expr, Shd);
+  float ver = 0.2;
+  printf("Shd Version %.1f \n",ver);
+  puts("Press Ctrl+c to Exit\n");
+  
+  while (1) {
+  
+    char* input = readline("Shd> ");
+    add_history(input);
+    
+    /* Attempt to parse the user input */
+    mpc_result_t r;
+    if (mpc_parse("<stdin>", input, Shd, &r)) {
+      /* On success print and delete the AST */
+      long result = eval(r.output);
+      printf("%li\n", result);
+      mpc_ast_delete(r.output);
+    } else {
+      /* Otherwise print and delete the Error */
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
+    
+    free(input);
+  }
+  
+  /* Undefine and delete our parsers */
+  mpc_cleanup(4, Number, Operator, Expr, Shd);
+  
   return 0;
 }
+
--- readme.md
+++ readme.md
@@ -11,10 +11,25 @@ run : `cc -std=c99 -Wall main.c -o main`
 
 - read user input .
 - callback to user with a message using `editline/readline.h` & `editline/history.h` .
-- now application scale on multiple platforms like windows, mac & linux .
+- now language scale on multiple platforms like windows, mac & linux .
 
 #### build
 
 run : `cc -std=c99 -Wall $main.c -ledit -o main`
 
 - make sure you installed the editline lib. with `sudo apt-get install libedit-dev`
+
+# Version 0.3
+
+- adding grammar to language .
+- language do a +, - , *, / , ^ operations and output the result to user .
+
+#### build
+
+- Linux & Mac
+
+run : `cc -std=c99 -Wall main.c mpc.c -ledit -lm -o main`
+
+- Windows
+
+run : `cc -std=c99 -Wall main.c mpc.c -o main`
